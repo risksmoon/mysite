5bd505f3f7e788655fd34b7087a3920e4d21195a
risksmoon <negativepower100@gmail.com>
2026-09-08 14:59:08 +0500

    Add files via upload

---
+++ app.py
@@ -0,0 +1,16 @@
+from flask import Flask, render_template, request
+
+app = Flask(__name__)
+
+@app.route("/", methods=["GET", "POST"])
+def home():
+    name = ""
+    age = ""
+
+    if request.method == "POST":
+        name = request.form["name"]
+        age = request.form["age"]
+
+    return render_template("index.html", name=name, age=age)
+
+app.run(debug=True)
